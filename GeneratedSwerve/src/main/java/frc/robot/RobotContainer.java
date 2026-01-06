75651e4d052128f4417cd75dfc07b8af2e92920c
amory-kwarqs <amory.kwarqs@gmail.com>
2026-01-05 20:54:41 -0500

    tried to fix an error where ball gets stuck in intake - not fully fixed

--- GeneratedSwerve/src/main/java/frc/robot/RobotContainer.java
+++ GeneratedSwerve/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -26,99 +27,114 @@ import frc.robot.subsystems.FunnelingSubsystem;
 
 public class RobotContainer {
 
-    private final FunnelingSubsystem funneling = new FunnelingSubsystem();
-    private final ElevatorFunnelSubsystem elevatorFunnel = new ElevatorFunnelSubsystem();
+        private final FunnelingSubsystem funneling = new FunnelingSubsystem();
+        private final ElevatorFunnelSubsystem elevatorFunnel = new ElevatorFunnelSubsystem();
 
-    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
-                                                                                      // max angular velocity
+        private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
+                                                                                      // speed
+        private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
+                                                                                          // second
+                                                                                          // max angular velocity
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+        /* Setting up bindings for necessary control of the swerve drive platform */
+        private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+                        .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
+                                                                                 // motors
+        private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+        private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+        private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController driver = new CommandXboxController(0);
+        private final CommandXboxController driver = new CommandXboxController(0);
 
-    public final IntakeSubsystem intake = new IntakeSubsystem();
+        public final IntakeSubsystem intake = new IntakeSubsystem();
 
-    public final ShooterSubsystem shooter = new ShooterSubsystem();
+        public final ShooterSubsystem shooter = new ShooterSubsystem();
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+        public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-    public RobotContainer() {
-        configureBindings();
-    }
+        public RobotContainer() {
+                configureBindings();
+                SmartDashboard.putData("IntakeSubsystem", intake);
+        }
 
-    private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-                // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
-                                                                                                 // negative Y (forward)
-                        .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with
-                                                                                  // negative X (left)
-                ));
+        private void configureBindings() {
+                // Note that X is defined as forward according to WPILib convention,
+                // and Y is defined as to the left according to WPILib convention.
+                drivetrain.setDefaultCommand(
+                                // Drivetrain will execute this command periodically
+                                drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive
+                                                                                                                 // forward
+                                                                                                                 // with
+                                                                                                                 // negative
+                                                                                                                 // Y
+                                                                                                                 // (forward)
+                                                .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with
+                                                                                              // negative X (left)
+                                                .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive
+                                                                                                          // counterclockwise
+                                                                                                          // with
+                                                                                                          // negative X
+                                                                                                          // (left)
+                                ));
 
-        // Idle while the robot is disabled. This ensures the configured
-        // neutral mode is applied to the drive motors while disabled.
-        final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-                drivetrain.applyRequest(() -> idle).ignoringDisable(true));
+                // Idle while the robot is disabled. This ensures the configured
+                // neutral mode is applied to the drive motors while disabled.
+                final var idle = new SwerveRequest.Idle();
+                RobotModeTriggers.disabled().whileTrue(
+                                drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-        driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        // driver.b().whileTrue(drivetrain.applyRequest(() ->
-        // // point.withModuleDirection(new Rotation2d(-driver.getLeftY(),
-        // -driver.getLeftX()))
-        // point.withModuleDirection(new Rotation2d(0))
-        // ));
+                driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+                // driver.b().whileTrue(drivetrain.applyRequest(() ->
+                // // point.withModuleDirection(new Rotation2d(-driver.getLeftY(),
+                // -driver.getLeftX()))
+                // point.withModuleDirection(new Rotation2d(0))
+                // ));
 
-        driver.b().whileTrue(Commands.parallel(
-                funneling.funnelIn(),
-                elevatorFunnel.elevatorFunnelIn()))
-                .onFalse(funneling.funnelStop().andThen(elevatorFunnel.elevatorFunnelStop()));
+                driver.b().whileTrue(Commands.parallel(
+                                funneling.funnelIn(),
+                                elevatorFunnel.elevatorFunnelIn()))
+                                .onFalse(funneling.funnelStop().andThen(elevatorFunnel.elevatorFunnelStop()));
 
-        new Trigger(elevatorFunnel::seesBall).whileTrue(ballUpLift());
+                new Trigger(elevatorFunnel::seesBall).whileTrue(ballUpLift());
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        driver.back().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        driver.back().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        driver.start().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+                // Run SysId routines when holding back/start and X/Y.
+                // Note that each routine should be run exactly once in a single log.
+                driver.back().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+                driver.back().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+                driver.start().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+                driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        // reset the field-centric heading on left bumper press
-        driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+                // reset the field-centric heading on left bumper press
+                driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-        driver.button(9).whileTrue(Commands.parallel(intake.intakeIn(), funneling.funnelIn()))
-                .onFalse(intake.intakeStop().andThen(() -> funneling.funnelStop()));
-        driver.button(10).whileTrue(intake.intakeOut()).onFalse(intake.intakeStop());
+                driver.button(9).whileTrue(Commands.parallel(intake.intakeIn(), funneling.funnelIn()))
+                                .onFalse(intake.intakeStop().andThen(() -> funneling.funnelStop()));
 
-        driver.leftTrigger().whileTrue(Commands.parallel(shooter.shooterFast(), elevatorFunnel.elevatorFunnelIn()))
-                .onFalse(shooter.shooterStop().andThen(elevatorFunnel.elevatorFunnelStop()));
-        driver.rightTrigger().whileTrue(Commands.parallel(shooter.shooterSlow(), elevatorFunnel.elevatorFunnelIn()))
-                .onFalse(shooter.shooterStop().andThen(elevatorFunnel.elevatorFunnelStop()));
+                driver.button(10).whileTrue(intake.intakeOut()).onFalse(intake.intakeStop());
 
-        drivetrain.registerTelemetry(logger::telemeterize);
-    }
+                driver.leftTrigger()
+                                .whileTrue(Commands.parallel(shooter.shooterFast(), elevatorFunnel.elevatorFunnelIn()))
+                                .onFalse(shooter.shooterStop().andThen(elevatorFunnel.elevatorFunnelStop()));
+                driver.rightTrigger()
+                                .whileTrue(Commands.parallel(shooter.shooterSlow(), elevatorFunnel.elevatorFunnelIn()))
+                                .onFalse(shooter.shooterStop().andThen(elevatorFunnel.elevatorFunnelStop()));
 
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+                drivetrain.registerTelemetry(logger::telemeterize);
+        }
 
-    public Command ballUpLift() {
-        return Commands.sequence(
-                Commands.parallel(
-                        funneling.funnelIn(),
-                        elevatorFunnel.elevatorFunnelIn()),
-                Commands.waitUntil(() -> elevatorFunnel.seesBall() == false),
-                funneling.funnelStop(),
-                elevatorFunnel.elevatorFunnelStop());
-    }
+        public Command getAutonomousCommand() {
+                return Commands.print("No autonomous command configured");
+        }
+
+        public Command ballUpLift() {
+                return Commands.sequence(
+                                Commands.parallel(
+                                                funneling.funnelIn(),
+                                                elevatorFunnel.elevatorFunnelIn()),
+                                Commands.waitUntil(() -> elevatorFunnel.seesBall() == false),
+                                funneling.funnelStop(),
+                                elevatorFunnel.elevatorFunnelStop());
+        }
 }
--- GeneratedSwerve/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ GeneratedSwerve/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -11,36 +11,73 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 
 public class IntakeSubsystem extends SubsystemBase {
-    
+
     private SparkFlex intake = new SparkFlex(18, MotorType.kBrushless);
 
     public IntakeSubsystem() {
         setDefaultCommand(intakeStop());
     }
 
+    public Command intakeCheckBroken() {
+        var command = runOnce(() -> intake.set(0.5));
+        command.setName("nothing wrong");
+
+        if (intake.getOutputCurrent() > 0.2 && intake.get() <= 0.2) {
+            var trueCommand = run(() -> intake.set(-0.5));
+            trueCommand.setName("Intake Out");
+            return trueCommand;
+        }
+        return command;
+    }
+
     public Command intakeIn() {
-        var command = run(()->intake.set(0.5));
+        var command = run(() -> intake.set(0.5)).until(() -> {
+            if (intake.getOutputCurrent() > 90 /* && intake.get() <= 0.2 */) {
+                return true;
+            }
+            return false;
+        }).andThen(run(() -> intake.set(-0.5)));
+
         command.setName("Intake In");
+
+        // if (intake.getOutputCurrent() > 0.2 && intake.get() <= 0.2) {
+        // var trueCommand = run(() -> intake.set(-0.5));
+        // trueCommand.setName("Intake Out");
+
+        // System.out.println("MADE IT AHHH");
+        // return trueCommand;
+        // }
 
         return command;
     }
 
     public Command intakeOut() {
-        var command = run(()->intake.set(-0.5));
+        var command = run(() -> intake.set(-0.5));
         command.setName("Intake Out");
 
         return command;
     }
 
     public Command intakeStop() {
-        var command = runOnce(()->intake.set(0));
+        var command = runOnce(() -> intake.set(0));
         command.setName("Intake Stop");
 
         return command;
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        // This is used to add things to NetworkTables
+        super.initSendable(builder);
+
+        builder.addDoubleProperty("intakeSpeed", () -> intake.get(), null);
+        builder.addDoubleProperty("intakeCurrent", () -> intake.getOutputCurrent(), null);
     }
 }
