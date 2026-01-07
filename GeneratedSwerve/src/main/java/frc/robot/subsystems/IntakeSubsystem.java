290934d89ab23b6abe18a37a4c994ef21901e479
amory-kwarqs <amory.kwarqs@gmail.com>
2026-01-07 18:46:19 -0500

    Added current limits to intake subsystem

--- GeneratedSwerve/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ GeneratedSwerve/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -14,22 +14,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkFlexConfig;
 
 public class IntakeSubsystem extends SubsystemBase {
 
     private SparkFlex intake = new SparkFlex(18, MotorType.kBrushless);
+    SparkFlexConfig motorConfig = new SparkFlexConfig();
 
     public IntakeSubsystem() {
+        setCurrentLimit(60, 40);
         setDefaultCommand(intakeStop());
+    }
+
+    private void setCurrentLimit(int stallLimit, int freeLimit) {
+        motorConfig.smartCurrentLimit(stallLimit, freeLimit);
+        intake.configureAsync(motorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
     }
 
     public Command intakeCheckBroken() {
         var command = runOnce(() -> intake.set(0.5));
         command.setName("nothing wrong");
 
-        if (intake.getOutputCurrent() > 0.2 && intake.get() <= 0.2) {
+        if (intake.getOutputCurrent() > 55 /* && intake.get() <= 0.2 */) {
             var trueCommand = run(() -> intake.set(-0.5));
             trueCommand.setName("Intake Out");
             return trueCommand;
@@ -39,7 +49,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public Command intakeIn() {
         var command = run(() -> intake.set(0.5)).until(() -> {
-            if (intake.getOutputCurrent() > 90 /* && intake.get() <= 0.2 */) {
+            if (intake.getOutputCurrent() > 55 /* && intake.get() <= 0.2 */) {
                 return true;
             }
             return false;
